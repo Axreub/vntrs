9634d635111a3581eb85ffdb856f6e694c959838
Intern <intern@Interns-MacBook-Pro.local>
2021-02-02 09:35:40 +0100

    minor refactoring

--- main.py
+++ main.py
@@ -13,38 +13,37 @@ import json
 from pprint import pprint
 url = "https://app.officevibe.com/api/v2/engagement"
 
-dates = []
-t = date.today()
-for i in range(0,7):
-	dates.append((t-timedelta(days=i)).strftime('%Y-%m-%d'))
 
-body = {
-	"dates": dates
-}
-headers = {
-    "Authorization": "Bearer 978d6683fb74430caac437ddb9dfd7322c0c2807f91a4bd89b255d9a8a74203ccf076ede9fde4fa286f0c81ab24987d1",
-    "Content-Type": "application/json"
-}
-
-valuearr = []
-
-r = requests.post(url, data=json.dumps(body), headers=headers)
-reports = r.json()["data"]["weeklyReports"]
+NUMBER_OF_DAYS = 7
 
 
+def get_officevibe():
+    headers = {
+        "Authorization": "Bearer 978d6683fb74430caac437ddb9dfd7322c0c2807f91a4bd89b255d9a8a74203ccf076ede9fde4fa286f0c81ab24987d1",
+        "Content-Type": "application/json"
+    }
+    dates = []
+    for i in range(0, NUMBER_OF_DAYS):
+        dates.append((date.today()-timedelta(days=i)).strftime('%Y-%m-%d'))
+    body = {
+        "dates": dates
+    }
 
-for i in reports:
-	for j in i["metricsValues"]:
-		j["date"]=i["date"]
-		valuearr.append(j)
+    return requests.post(url, data=json.dumps(body), headers=headers)
 
-pprint(valuearr)
+
+def stage_data(r):
+    valuearr = []
+    reports = r.json()["data"]["weeklyReports"]
+    for i in reports:
+        for j in i["metricsValues"]:
+            j["date"] = i["date"]
+            valuearr.append(j)
+
 
 app = FastAPI()
 
-'''
-@app.post("/")
-def read_root():
-    return {"Hello": "World"}
-'''
 
+@app.post("/")
+def post_root():
+    return("hello world")
